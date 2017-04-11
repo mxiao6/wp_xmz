3b76a0c50c170d6404187e7657c43b0d1891f0de
Mingze Xiao <mxiao666@gmail.com>
2017-04-11 17:30:16 -0500

    add theme

---
+++ wp-content/themes/panoramic/library/includes/dynamic-css.php
@@ -0,0 +1,15 @@
+<style type="text/css">
+
+<?php
+if ( $panoramic_slider_has_min_width ) {
+?>
+
+.slider-container.default .slider .slide img {
+	min-width: <?php echo floatVal( $panoramic_slider_min_width ); ?>px;
+}
+	
+<?php
+}
+?>
+
+</style>
